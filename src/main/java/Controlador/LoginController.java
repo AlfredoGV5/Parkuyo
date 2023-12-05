d7e67ee131347e46175d54b006c90c068b1dd51a
AlfredoGV5 <jgutierrezv1901@alumno.ipn.mx>
2023-12-05 15:00:53 -0600

    Logramos conectar la BD y hacer Login:D

--- src/main/java/Controlador/LoginController.java
+++ src/main/java/Controlador/LoginController.java
@@ -3,28 +3,46 @@ package Controlador;
 import BD.ConnectionBD;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class LoginController {
+    public TextField user;
+    public PasswordField password;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        String bases="";
-        try{
-            Statement sql= ConnectionBD.getConexion().createStatement();
-            String consulta="SELECT contraseña from usuario";
-            ResultSet resultado= sql.executeQuery(consulta);
-            while(resultado.next()){
-                System.out.println("hola");
-                bases += resultado.getString(1);
+    protected void buttonLogin() {
+        String usuario=user.getText();
+        String passwor= password.getText();
+        String consulta= "SELECT contraseña from usuario where usuario=? ";
+        try(PreparedStatement preparedStatement= ConnectionBD.getConexion().prepareStatement(consulta)){
+            preparedStatement.setString(1,usuario);
+            ResultSet resultado = preparedStatement.executeQuery();
+            //Verificamos que si exista una consulta que hacer (Que si exista el usuario)
+            if (resultado.next()) {
+                // Obtener la contraseña almacenada en la base de datos
+                String contraseñaBD = resultado.getString("contraseña");
+                // Comparar la contraseña ingresada con la almacenada
+                if (passwor.equals(contraseñaBD)) {
+                    welcomeText.setText("¡Inicio de sesión exitoso!");
+                } else {
+                    welcomeText.setText("Contraseña incorrecta");
+                }
+            } else {
+                // No se encontró el usuario
+                welcomeText.setText("Usuario no encontrado");
             }
-            System.out.println(bases);
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
